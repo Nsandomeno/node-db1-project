c6aa415c85a552e60cbdff5ff718a3af589f54fe
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-17 17:26:58 -0500

    Building endpoints.

---
+++ api/accountRouter.js
@@ -0,0 +1,11 @@
+// import express
+const express = require('express');
+// database access using knex
+const db = require('../data/dbConfig.js')
+// create router 
+const router = express.Router();
+
+// route handlers
+
+// export
+module.exports = router
